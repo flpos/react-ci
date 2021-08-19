d799f8bbc1a0a5015d091c62e441433c2bb986b2
Felipe Oliveira <fsousa@tasken.com>
2021-08-18 21:23:25 -0300

    test: add a broken test to force Workflow to fail

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -4,5 +4,10 @@ describe('sum function', () => {
   it('sum two numbers', () => {
     const result = sum(1, 2);
     expect(result).toBe(3);
-  })
+  });
+
+  it('runs a broken test', () => {
+    const result = sum(2, 3);
+    expect(result).toBe(23)
+  });
 })
